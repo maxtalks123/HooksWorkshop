04bf54614f82799a28f0bafb592c36a271d6e0aa
Max Emerson <maxemerson2013@gmail.com>
2024-02-13 14:19:31 +0000

    finished workshop and added extras with cookie styling

---
+++ UseState/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+import Cookie from "./Cookie";
+
+export default function App() {
+  const [myState, SetMyState] = useState(0);
+
+  function incrementState() {
+    SetMyState(myState + 1);
+  }
+
+  function decrementState() {
+    SetMyState(myState - 1);
+  }
+
+  function resetState() {
+    SetMyState(0);
+  }
+  return (
+    <div>
+      <Cookie />
+      <p onClick={resetState}>{myState} Cookies!</p>
+      <button onClick={incrementState}>Click to get more cookies</button>
+
+      <button onClick={decrementState}>Click to get one less cookie</button>
+      <button onClick={resetState}>Reset button</button>
+    </div>
+  );
+}
---
+++ UseState/src/Cookie.jsx
@@ -0,0 +1,13 @@
+import "./Cookie.css";
+
+export default function createCookie() {
+  return (
+    <div>
+      <h2>Click below for a giant cookie!</h2>
+      <img
+        src="https://clipground.com/images/cookie-clipart-icon-1.png"
+        alt="a giant cookie"
+      ></img>
+    </div>
+  );
+}
